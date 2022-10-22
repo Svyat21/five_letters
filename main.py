b64ad8abba5d5c80c311ad7f4595db97096c4c81
Svyatoslav Surovenkov <sursvyat@gmail.com>
2022-10-22 17:12:37 +0300

    adjusting the app launch settings

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ from src.routes import routes
 app = Starlette(debug=False, routes=routes)
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host='0.0.0.0', port=8000)
